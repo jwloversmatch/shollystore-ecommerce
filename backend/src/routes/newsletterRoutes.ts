e9c531ef50ec0aa976823ac708913af0ad6e8827
Jeremiah <jeze5959@gmail.com>
2026-09-09 01:09:38 +0100

    Added newsletter endpoint

--- backend/src/routes/newsletterRoutes.ts
+++ backend/src/routes/newsletterRoutes.ts
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import express from "express";
 import {
   subscribeToNewsletter,
   unsubscribeFromNewsletter,
 } from "../controllers/newsletterController";
 
-const router = Router();
+const router = express.Router();
 
 router.post("/subscribe", subscribeToNewsletter);
 router.post("/unsubscribe", unsubscribeFromNewsletter);
