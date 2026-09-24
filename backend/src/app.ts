fd1c75f1dc4dbaaf25800216661b131022902f1e
Jeremiah <jeze5959@gmail.com>
2026-09-25 00:16:35 +0100

    Added new domain

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -35,6 +35,7 @@ const allowedOrigins = [
   ...(process.env.ALLOWED_ORIGINS || "").split(","),
   "https://sholexstore.com",
   "https://www.sholexstore.com",
+  "https://sholex.vercel.app",
   "http://localhost:5173",
   "http://localhost:3000",
   "http://127.0.0.1:5173",
